a6454bdb7353a62b757c94d7d9702550efc5a9db
TharinduBasnayaka <tharindupramuditha2@gmail.com>
2025-02-07 16:38:14 +0530

    manged to connect to a postgres DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,36 +4,53 @@ import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
 import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
 import { SongsController } from './songs/songs.controller';
-import { DevConfigServices } from './common/providers/DevConfigServices';
+//import { DevConfigServices } from './common/providers/DevConfigServices';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
-const devConfig = {
-  port: 3000,
-};
-const proConfig = {
-  port: 4000,
-};
+// const devConfig = {
+//   port: 3000,
+// };
+// const proConfig = {
+//   port: 4000,
+// };
 @Module({
-  imports: [SongsModule],
+  imports: [
+    SongsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: 'spotify_clone',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [
     AppService,
     //this is a example of a class based providers
-    {
-      provide: DevConfigServices,
-      useClass: DevConfigServices,
-    },
+    // {
+    //   provide: DevConfigServices,
+    //   useClass: DevConfigServices,
+    // },
     //example ends
     //example for factory provider
-    {
-      provide: 'CONFIG',
-      useFactory: () => {
-        return process.env.NODE_ENV === 'development' ? devConfig : proConfig;
-      },
-    },
+    // {
+    //   provide: 'CONFIG',
+    //   useFactory: () => {
+    //     return process.env.NODE_ENV === 'development' ? devConfig : proConfig;
+    //   },
+    // },
     //example ends
   ],
 })
 export class AppModule implements NestModule {
+  constructor(private dataSource: DataSource) {
+    console.log('dbname: ', dataSource.driver.database);
+  }
   //using the logger middleware in the AppModule
   configure(consumer: MiddlewareConsumer) {
     //consumer.apply(LoggerMiddleware).forRoutes('songs'); //applying middleware - method no 1, only run for the specific route
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,14 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { DevConfigServices } from './common/providers/DevConfigServices';
+import { Injectable } from '@nestjs/common';
+//import { DevConfigServices } from './common/providers/DevConfigServices';
 
 @Injectable()
 export class AppService {
-  constructor(
-    private devConfigService: DevConfigServices,
-    @Inject('CONFIG') private config: { port: string },
-  ) {}
+  // constructor(
+  //   private devConfigService: DevConfigServices,
+  //   @Inject('CONFIG') private config: { port: string },
+  // ) {}
   getHello(): string {
-    return `Hello World! DBHOST from DevConfigService ${this.devConfigService.getDbHost()} in port ${this.config.port}`;
+    return 'hello world!';
+    // return `Hello World! DBHOST from DevConfigService ${this.devConfigService.getDbHost()} in port ${this.config.port}`;
   }
 }
